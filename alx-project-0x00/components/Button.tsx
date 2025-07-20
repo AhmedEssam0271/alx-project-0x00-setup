f80ab0a50bb2bc70fff40a975eb8f8f43185d5e1
AhmedEssam0271 <ahmedessam0271@gmail.com>
2025-07-20 22:06:23 +0300

    Advanced Task
    Objective: Create a button component that accepts title and styles as an argument. Create multiple instances (sizes: small, medium, Large - shapes: rounded-sm, rounded-md, rounded-full) of the button component under the /landing paths.

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,12 @@
+import { ButtonProps } from "@/interfaces";
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+  return (
+    <button
+      className={`border px-3 py-1 text-lg mt-3  bg-blue-500 text-white ${styles}`}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,16 @@
 import Card from "@/components/Card";
+import Button from "@/components/Button";
+
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+      <div className="flex flex-col items-center">
+        <Button title="Small Rounded-sm" styles="text-sm rounded-sm" />
+        <Button title="Medium Rounded-md" styles="text-md rounded-md" />
+        <Button title="Large Rounded-full" styles="text-lg rounded-full" />
+      </div>
     </div>
   );
 };
